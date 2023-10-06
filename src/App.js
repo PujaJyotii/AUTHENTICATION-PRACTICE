ef38d431e614655c558fddd2b6e12f6cfc1b34d9
PujaJyotii <pujajyoti111>
2023-10-06 14:26:12 +0530

    changes for logout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
+import { useContext } from 'react';
+import AuthContext from './Store/auth-context';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCtx.isLoggedIn && (<Route path='/auth'>
           <AuthPage />
+        </Route>)}
+        <Route path='/profile'> {authCtx.isLoggedIn && <ProfilePage /> }
+        {!authCtx.isLoggedIn  && <Redirect to='/auth' />}
         </Route>
-        <Route path='/profile'>
-        <ProfilePage />
+        <Route path='*'>
+          <Redirect to='/'/>
         </Route>
       </Switch>
     </Layout>
